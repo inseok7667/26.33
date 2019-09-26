a7e9ee9530feef3d3c0e103c35f274301beadd33
inseok7667 <inseok7667@gmail.com>
2019-09-26 19:48:40 +0900

    Add Youtube Movie-Trailer

--- MoviTest/app/src/main/java/com/example/movitest/DetailMovie.java
+++ MoviTest/app/src/main/java/com/example/movitest/DetailMovie.java
@@ -9,13 +9,20 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import org.w3c.dom.Text;
 
-public class DetailMovie extends Activity {
+public class DetailMovie extends YouTubeBaseActivity {
     private SimpleDraweeView poster_detail;
     private TextView title_detail;
     private TextView overview_detail;
+    private YouTubePlayerView youtubeView;
+    YouTubePlayer.OnInitializedListener playListener;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +32,32 @@ public class DetailMovie extends Activity {
         poster_detail = findViewById(R.id.poster_detail);
         title_detail = findViewById(R.id.title_detail);
         overview_detail = findViewById(R.id.overview_detail);
+        youtubeView = findViewById(R.id.youtubeView);
+
+
+
+        playListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("4vWg5yJuWfs");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        youtubeView.initialize("AIzaSyB4gLjh8U0QvwAyl_gStzUHUe2MbNuFXrY",playListener);
+
+
+
 
         Intent intent = getIntent();
 
         String dtitle = intent.getStringExtra("title");
         String dposeter = intent.getStringExtra("poster_path");
         String doverview = intent.getStringExtra("overview");
+        String t_id;
 
         Log.d("sre",dtitle);
         Log.d("sre",dposeter);
@@ -38,8 +65,15 @@ public class DetailMovie extends Activity {
         Uri uri = Uri.parse("https://image.tmdb.org/t/p/w500/"+dposeter);
 
         poster_detail.setImageURI(uri);
-        title_detail.setText("영화 제목"+dtitle);
+        title_detail.setText("영화 제목:"+dtitle);
         overview_detail.setText("영화내용:"+doverview);
+
+
+
+
+
+
+
 
 
 
